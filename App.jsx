0a35fc531da4228948c0e7d8038a1cb1566db3ad
Johnpaul-225 <157563274+Johnpaul-225@users.noreply.github.com>
2025-12-15 11:27:39 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,22 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
+import Login from "./pages/Login"
+import Register from "./pages/Register"
+import Dashboard from "./pages/Dashboard"
+import Navbar from "./components/Navbar"
+
+function App() {
+  const token = localStorage.getItem("token")
+
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={token ? <Dashboard /> : <Navigate to="/login" />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
